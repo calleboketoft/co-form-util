3eeb4a794c2a53d3b126d0a11444ea1711948d7f
Carl-Ola Boketoft <calleboketoft@gmail.com>
2016-08-03 13:03:34 +0200

    export util properly

---
+++ co-form-util.js
@@ -0,0 +1,6 @@
+"use strict";
+var error_message_component_1 = require('./client-src/error-message.component');
+exports.ErrorMessageComponent = error_message_component_1.ErrorMessageComponent;
+var form_group_component_1 = require('./client-src/form-group.component');
+exports.FormGroupComponent = form_group_component_1.FormGroupComponent;
+//# sourceMappingURL=co-form-util.js.map
---
+++ co-form-util.ts
@@ -0,0 +1,2 @@
+export {ErrorMessageComponent} from './client-src/error-message.component'
+export {FormGroupComponent} from './client-src/form-group.component'
