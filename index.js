7708f7bd9bef37708b50a7aecdccafd1c7c9ca13
Carl-Ola Boketoft <calleboketoft@gmail.com>
2017-03-30 11:08:22 +0200

    updated to angular 4.0.1

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 var form_tools_module_1 = require("./client-src/form-tools.module");
 exports.FormToolsModule = form_tools_module_1.FormToolsModule;
 //# sourceMappingURL=index.js.map
